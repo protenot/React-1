127deb99b9a7dedae7c751c05858c7dcf381d373
Olga_Belaya <protenot@yandex.ru>
2023-09-14 20:02:45 +0300

    doesn't work storybook

--- src/components/Image/image.story.tsx
+++ src/components/Image/image.story.tsx
@@ -1,50 +1,36 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Image } from './image';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button';
+
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Image',
+  component: Image,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered',
+    layout: 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' }, 
-  },
-} satisfies Meta<typeof Button>;
+  /* argTypes: {
+    backgroundColor: { control: 'black' },
+  }, */
+} satisfies Meta<typeof Image>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+export const Primary = {
+    src:'../../img/Kittens.jpg',
+    alt:"Kittens",
+        width:200,
+        height:300
+}
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
 
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
