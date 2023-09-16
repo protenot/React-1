268701437047601fd82f6a01cc148955acf723c1
Olga_Belaya <protenot@yandex.ru>
2023-09-16 13:39:24 +0300

    now components render in any order and any quantity in storybook

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -10,7 +10,7 @@ import { TextWithSelect } from '../Text/text1.stories';
 import { UnfoldingBlock } from '../UnfoldingBlock/block.stories';
 const Img = require ('../../img/Kittens.jpg');
 
-const options = [
+ /* const options = [
     { value: "", text: "--Choose an option--" },
     { value: "Header", text: "Header" },
     { value: "Text", text: "Text" },
@@ -18,9 +18,9 @@ const options = [
     { value: "UnfoldingBlock", text: "UnfoldingBlock" },
     { value: "Image", text: "Image" },
     { value: "Nothing", text: "Nothing" },
-  ];
+  ];  */
 
-type Story = StoryObj<typeof Select>;
+//type Story = StoryObj<typeof Select>;
 export default {
     title: "Component/Select",
     component: Select,
@@ -37,22 +37,60 @@ export default {
           if (selectedValue === "Nothing") {
             // Если выбрано "Nothing", очистить все выбранные компоненты
             setSelectedComponents([]);
-          } else if (!selectedComponents.includes(selectedValue)) {
+          } else 
+          //if (!selectedComponents.includes(selectedValue))
+           {
             // Если компонент еще не выбран, добавить его в список
             setSelectedComponents([...selectedComponents, selectedValue]);
           }
         };
-      
+        const renderBlock = (blockType: string) => {
+          switch (blockType) {
+            case "Header":
+              return (
+                <>
+                  <HeaderWithInput value={0} />
+                </>
+              );
+            case "Text":
+              return (
+                <TextWithSelect />
+              );
+            case "Break":
+              return <Break />;
+            case "UnfoldingBlock":
+              return <UnfoldingBlock></UnfoldingBlock>;
+            case "Image":
+              return (
+                <Image src={Img} alt={""} width={200} height={300} />
+              );
+            case "Nothing":
+              return <></>;
+            default:
+              return null;
+          }
+        };
         return (
+          <>
+            <Select onSelectChange={onSelectChange}></Select>
+            {selectedComponents.map((block, index) => (
+              <div key={index}>{renderBlock(block)}</div>
+            ))}
+          </>
+        );
+
+      
+      /*   return (
           <>
             <Select onSelectChange={onSelectChange} />
             {selectedComponents.includes("Header") && <HeaderWithInput value={0} />}
             {selectedComponents.includes("Break") && <Break />}
+           
+            {selectedComponents.includes("Text") && <TextWithSelect />}
+            {selectedComponents.includes("UnfoldingBlock") && <UnfoldingBlock />}
             {selectedComponents.includes("Image") && (
               <Image src={Img} alt={""} width={200} height={300} />
             )}
-            {selectedComponents.includes("Text") && <TextWithSelect />}
-            {selectedComponents.includes("UnfoldingBlock") && <UnfoldingBlock />}
           </>
-        );
+        ); */
       };
